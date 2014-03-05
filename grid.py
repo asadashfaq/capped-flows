3fa9bf9c6d0322f522e70b00bbeb9baba617eb0c
Bo Tranberg <bo@tberg.dk>
2014-03-05 14:53:21 +0100

    simplifications

--- grid.py
+++ grid.py
@@ -1,10 +1,6 @@
 #! /usr/bin/env python
-import os
 import aurespf.solvers as au
 import numpy as np
-from pylab import plt
-import multiprocessing as mp
-from aurespf.tools import *
 
 # EU grid with all alphas set to .7
 
@@ -14,12 +10,3 @@ files = ['AT.npz', 'FI.npz', 'NL.npz', 'BA.npz', 'FR.npz', 'NO.npz', 'BE.npz', '
 
 def EU_Nodes(load_filename=None, full_load=False):
     return au.Nodes(admat='./settings/eadmat.txt', path='./data/', prefix = "ISET_country_", files=files, load_filename=load_filename, full_load=full_load, alphas=alphas, gammas=np.ones(30))
-
-
-N = EU_Nodes()
-caps = get_quant_caps(quant=.99)
-
-for b in np.linspace(.1,1,10):
-    N,F = au.solve(N,mode='capped martin verbose',h0=caps,b=b)
-    N.save_nodes('martin-b-'+str(b))
-    np.save('./results/martinflows-b-'+str(b),F)
