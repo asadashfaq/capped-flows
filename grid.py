71eee46065784b85646579cd2554efc731b4ceb8
Bo <bo@pepsi.imf.au.dk>
2014-03-05 11:05:14 +0100

    firstÃ

---
+++ grid.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+import os
+import aurespf.solvers as au
+import numpy as np
+from pylab import plt
+import multiprocessing as mp
+
+alphas = np.ones(30)*.7
+
+files = ['AT.npz', 'FI.npz', 'NL.npz', 'BA.npz', 'FR.npz', 'NO.npz', 'BE.npz', 'GB.npz', 'PL.npz', 'BG.npz', 'GR.npz', 'PT.npz', 'CH.npz', 'HR.npz', 'RO.npz', 'CZ.npz', 'HU.npz', 'RS.npz', 'DE.npz', 'IE.npz', 'SE.npz', 'DK.npz', 'IT.npz', 'SI.npz', 'ES.npz', 'LU.npz', 'SK.npz', 'EE.npz', 'LV.npz', 'LT.npz']
+
+def Nodes(load_filename=None, full_load=False):
+    return au.Nodes(admat='./settings/eadmat.txt', path='./data/', prefix = "ISET_country_", files=files, load_filename=load_filename, full_load=full_load, alphas=alphas, gammas=np.ones(30))
+
+    
