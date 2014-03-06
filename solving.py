4253096601abc31b2f01b186464f6c6bd89c7dd9
Bo Tranberg <bo@tberg.dk>
2014-03-06 11:56:53 +0100

    added plotting

--- solving.py
+++ solving.py
@@ -16,24 +16,46 @@ Solving different implementations of capped flow to produce Bc vs Tc figures
 if len(sys.argv)<2:
     raise Exception('No mode selected!')
 else:
-    mode = str(sys.argv[1:])
+    task = str(sys.argv[1])
+    if len(sys.argv)>2:
+        mode = str(sys.argv[2:])
 
-# Load data and get flow quantiles
-N = EU_Nodes()
-caps = get_quant_caps(quant=.99)
+if 'solve' in task:
+    # Load data and get flow quantiles
+    N = EU_Nodes()
+    caps = get_quant_caps(quant=.99)
 
-# Martin flow
-if 'martin' in mode:
-    print 'Solving capped Martin flow'
-    for b in np.linspace(.1,2,39):
-        N,F = au.solve(N,mode='capped martin verbose',h0=caps,b=b)
-        N.save_nodes('martin-b-'+str(b))
-        np.save('./results/martinflows-b-'+str(b),F)
+    # Martin flow
+    if 'martin' in mode:
+        print 'Solving capped Martin flow'
+        for b in np.linspace(.1,2,39):
+            N,F = au.solve(N,mode='capped martin verbose',h0=caps,b=b)
+            N.save_nodes('martin-b-'+str(b))
+            np.save('./results/martinflows-b-'+str(b),F)
 
-#Rolando flow
-if 'rolando' in mode:
-    print 'Solving capped Rolando flow'
-    for b in np.linspace(.1,2,39):
-        N,F = au.solve(N,mode='capped rolando verbose',h0=caps,b=b)
-        N.save_nodes('rolando-b-'+str(b))
-        np.save('./results/rolandoflows-b-'+str(b),F)
+    #Rolando flow
+    if 'rolando' in mode:
+        print 'Solving capped Rolando flow'
+        for b in np.linspace(.1,2,39):
+            N,F = au.solve(N,mode='capped rolando verbose',h0=caps,b=b)
+            N.save_nodes('rolando-b-'+str(b))
+            np.save('./results/rolandoflows-b-'+str(b),F)
+
+if 'plot' in task:
+    if 'martin' in mode:
+        mBc = []
+        qBc = []
+        Tc = []
+        for b in np.linspace(.1,2,39):
+            N = np.load('./results/martin-b-'+str(b)+'.npz')
+            F = np.load('./results/martinflows-b-'+str(b)+'.npy')
+            mBc.append(np.sum(np.max(N['load'],1)))
+            q=[]
+            for i in range(0,30):
+                q.append(get_q(N['load'][i]))
+            qBc.append(sum(q))
+            Tc.append(sum(biggestpair(np.max(abs(F),1))))
+    plt.figure()
+    plt.plot(Tc,mBc,'-b')
+    plt.plot(Tc,qBc,'-k')
+    plt.savefig('bctc.eps')
