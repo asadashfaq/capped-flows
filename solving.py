aacb2aa41d3996872a3e49839fa3eb2484556a96
Bo Tranberg <bo@tberg.dk>
2014-03-07 18:14:34 +0100

    now with normalization

--- solving.py
+++ solving.py
@@ -16,23 +16,35 @@ Examples to run the script from terminal:
 2) python solving.py plot martin
 """
 
-def Bc0(file):
-# Calculation of initial balancing constraints for normalisation
-    N = EU_Nodes(load_filename=file+'.npz')
-    mean_n = [node.mean for node in N]
-    Global_B = np.max(-sum(n.mismatch for n in N))
-    Bc0 = mean_n*Global_B/sum(mean_n)
-    print 'Bc0 = ',Bc0
-    print len(Bc0)
-    return Bc0
+def plotter(mode):
+    mBc = []
+    qBc = []
+    Tc = []
+    for b in np.linspace(.1,2,39):
+        N = EU_Nodes(str(mode)+'-b-'+str(b)+'.npz')
+        F = np.load('./results/'+str(mode)+'flows-b-'+str(b)+'.npy')
+        Tload = np.sum(np.sum(n.mean for n in N))
+        mBc.append(np.sum(np.max(n.balancing) for n in N)/Tload)
+        qBc.append(np.sum(get_q(n.balancing,.99) for n in N)/Tload)
+        Tc.append(sum(biggestpair(np.max(abs(F),1))))
+    plt.figure()
+    plt.plot(Tc,mBc,'-b')
+    plt.plot(Tc,qBc,'-k')
+    plt.xlabel('Tc')
+    plt.ylabel('Bc')
+    plt.title(str(mode)+' flow')
+    plt.savefig('bctc_'+str(mode)+'.eps')
 
 # Which implementation to solve
-if len(sys.argv)<2:
-    raise Exception('No mode selected!')
+if len(sys.argv)<3:
+    raise Exception('Not enough inputs!')
 else:
     task = str(sys.argv[1])
-    if len(sys.argv)>2:
-        mode = str(sys.argv[2:])
+    mode = str(sys.argv[2:])
+    if (('solve' not in task ) and ('plot' not in task)):
+        raise Exception('Wrong task selected!')
+    if (('martin' not in mode) and ('rolando' not in mode)):
+        raise Exception('No implementation selected!')
 
 if 'solve' in task:
     # Load data and get flow quantiles
@@ -57,47 +69,8 @@ if 'solve' in task:
 
 if 'plot' in task:
     if 'martin' in mode:
-# Put all of this in a function
         print 'Plotting capped Martin flow'
-        mBc = []
-        qBc = []
-        Tc = []
-        Bc0 = Bc0('martin-b-0.1')
-        for b in np.linspace(.1,2,39):
-            N = EU_Nodes('martin-b-'+str(b)+'.npz')
-            F = np.load('./results/martinflows-b-'+str(b)+'.npy')
-            mBc.append(np.sum(np.max(n.balancing)) for n in N) # remember normalisation
-            q=[]
-            for i in range(0,30): # This should be done in another way
-                q.append(get_q(N['load'][i]))
-            qBc.append(sum(q))
-            Tc.append(sum(biggestpair(np.max(abs(F),1))))
-        plt.figure()
-        plt.plot(Tc,mBc/Bc0,'-b')
-        plt.plot(Tc,qBc/Bc0,'-k')
-        plt.xlabel('Tc')
-        plt.ylabel('Bc')
-        plt.title('Martin flow')
-        plt.savefig('bctc_martin.eps')
+        plotter('martin')
 
     if 'rolando' in mode:
-        print 'Plotting capped Rolando flow'
-        mBc = []
-        qBc = []
-        Tc = []
-        for b in np.linspace(.1,2,39):
-            N = np.load('./results/rolando-b-'+str(b)+'.npz')
-            F = np.load('./results/rolandoflows-b-'+str(b)+'.npy')
-            mBc.append(np.sum(np.max(N['load'],1)))
-            q=[]
-            for i in range(0,30):
-                q.append(get_q(N['load'][i]))
-            qBc.append(sum(q))
-            Tc.append(sum(biggestpair(np.max(abs(F),1))))
-        plt.figure()
-        plt.plot(Tc,mBc,'-b')
-        plt.plot(Tc,qBc,'-k')
-        plt.xlabel('Tc')
-        plt.ylabel('Bc')
-        plt.title('Rolando flow')
-        plt.savefig('bctc_rolando.eps')
+        plotter('rolando')
